112e55f2a2f3de63462f8c7142141eaeb86f0054
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-03-21 15:41:04 +0900

    ベースレイアウト用のファイルを編集
    HTML部分を追記
    編集
    resources/views/layouts/step7app.blade.php

---
+++ tng-cte-s7and8/resources/views/layouts/step7app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="旧CyTech STEP7 テスト">
+    <meta name="author" content="Takumi Sakiya(Tokyo Branch)">
+    <title></title>
+    <link rel="stylesheet" href="{{asset('css/style.css)}}">
+</head>
+
+<body>
+</body>
+
+</html>
