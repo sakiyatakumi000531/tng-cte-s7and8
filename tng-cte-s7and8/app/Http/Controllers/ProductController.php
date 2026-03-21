8ff8af919ca5ca08892c65b251642d35e48ba34c
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-03-21 17:49:14 +0900

    ProductController.php に 全件取得機能を追加
    編集
    app\Http\Controllers\ProductController.php

--- tng-cte-s7and8/app/Http/Controllers/ProductController.php
+++ tng-cte-s7and8/app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $items = Product::all();
+        return view('product.index', ['items' => $items]);
+    }
 }
