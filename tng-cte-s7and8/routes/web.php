3cb6f826bfeff0532884c03f8b66deed6b4dc9d0
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-04-12 15:56:29 +0900

    ルート情報の編集
    該当ファイル
    routes/web.php

--- tng-cte-s7and8/routes/web.php
+++ tng-cte-s7and8/routes/web.php
@@ -10,4 +10,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/product', [ProductController::class, 'index'])->name('index');
