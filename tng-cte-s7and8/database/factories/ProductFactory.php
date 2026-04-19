f9adc0f1e42615c08bbfee515f2f3ea829153cf4
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-04-19 22:01:50 +0900

    Products テーブル用の Factory を編集
    理由
    サンプルデータ生成の準備をするため。

--- tng-cte-s7and8/database/factories/ProductFactory.php
+++ tng-cte-s7and8/database/factories/ProductFactory.php
@@ -18,7 +18,18 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            // 外部キー(bigint unsigned)
+            'company_id' => Company::factory(),
+            // 商品名(varchar)
+            'product_name' => $this->faker->word(),
+            // 価格(int)
+            'price' => $this->faker->numberBetween(100, 300),
+            // 在庫数(int)
+            'stock' => $this->faker->numberBetween(0, 999),
+            // コメント(text / Nullable)
+            'comment' => $this->faker->optional()->realText(100),
+            // 商品画像(varchar / Nullable) width: 640, height: 480, 画像のカテゴリー: 'products' の画像URL または Null
+            'img_path' => $this->faker->optional()->imageUrl(640, 480, 'products')
         ];
     }
 }
