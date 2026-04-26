9cedde28e42bf10ccbb8c896ee2fbf12cad5ca23
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-04-26 18:12:58 +0900

    各Modelにリレーションを設定した
    理由
    複数のテーブルにまたがった情報をまとめて取り出せるようにするため。
    
    作業ファイル
    modified:   app/Models/Company.php
    modified:   app/Models/Product.php
    modified:   app/Models/Sale.php

--- tng-cte-s7and8/app/Models/Product.php
+++ tng-cte-s7and8/app/Models/Product.php
@@ -6,7 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Productモデル
+ *
+ * Product : Sale = 1 : 多,
+ * Product : Company = 多 : 1
+ */
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * hasMany結合
+     *
+     * Product : Sale = 1 : 多
+     */
+    public function sales() {
+        return $this->hasMany('App\Models\Sale');
+    }
+
+    /**
+     * belongsTo結合
+     *
+     * Product : Company = 多 : 1
+     */
+    public function company() {
+        return $this->belongsTo('App\Models\Company');
+    }
 }
--- tng-cte-s7and8/app/Models/Sale.php
+++ tng-cte-s7and8/app/Models/Sale.php
@@ -4,7 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Saleモデル
+ *
+ * Sale : Product = 多 : 1
+ */
 class Sale extends Model
 {
-    //
+    /**
+     * belongsTo結合
+     *
+     * Sale : Product = 多 : 1
+     */
+    public function product() {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
