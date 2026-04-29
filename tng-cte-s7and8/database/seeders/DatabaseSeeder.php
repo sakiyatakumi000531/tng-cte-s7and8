9328d24a7c10d80d4ee9cda0f512b43da86e5d5c
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-04-29 18:30:43 +0900

    DatabaseSeederに各Factoryを呼び出す処理を追加
    理由
    recycle()を使用してデータの整合性を保つため
    
    作業ファイル
    modified:   database/seeders/DatabaseSeeder.php

--- tng-cte-s7and8/database/seeders/DatabaseSeeder.php
+++ tng-cte-s7and8/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,11 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// 自分でインポート
+use App\Models\Company;
+use App\Models\Product;
+use App\Models\Sale;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -15,11 +20,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // デフォルトユーザ
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // 自作ユーザ
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => 'user1234',
+        ]);
+
+        // 基準となる親を先に作る
+        $companies = Company::factory(20)->create();
+
+        /*
+        子を作る際、先ほど作成した $companies を再利用する
+        これにより、ProductFactory内の Company::factory() は無視され、
+        $companies の中からランダムにIDが割り振られる
+        */
+        $products = Product::factory(100)->recycle($companies)->create();
+
+        /*
+        子を作る際、先ほど作成した $products を再利用する
+        これにより、SaleFactory内の Product::factory() は無視され、
+        $products の中からランダムにIDが割り振られる
+        */
+        Sale::factory(300)->recycle($products)->create();
     }
 }
