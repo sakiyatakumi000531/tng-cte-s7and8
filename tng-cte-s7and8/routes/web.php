bc31fe8ae5f47617ec327009330b849b901c35bd
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-02 18:49:06 +0900

    テンプレートから渡される実引数を受け取る仮引数の値を修正
    理由
    * より適切な名前にするため。
    
    作業ファイル
    * modified:   routes/web.php

--- tng-cte-s7and8/routes/web.php
+++ tng-cte-s7and8/routes/web.php
@@ -24,13 +24,13 @@ Route::get('/product/create', [ProductController::class, 'create'])->name('produ
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 
 // 商品情報詳細
-Route::get('/product/{item}', [ProductController::class, 'show'])->name('product.show');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 
 // 商品情報編集(入力)
-Route::get('/product/{item}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
 
 // 商品情報編集(処理)
-Route::put('/product/{item}', [ProductController::class, 'update'])->name('product.update');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
 
 // 削除
-Route::delete('/product/{item}', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
