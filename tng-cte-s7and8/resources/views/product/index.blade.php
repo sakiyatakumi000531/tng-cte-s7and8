72264b6a1415068742c015ec1edf852f1d5651a7
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-02 22:26:12 +0900

    商品情報一覧ページに検索用入力フォームとセレクトボックスを追加
    理由
    * 検索機能の追加のため。
    
    作業ファイル
    * modified:   resources/views/product/index.blade.php

--- tng-cte-s7and8/resources/views/product/index.blade.php
+++ tng-cte-s7and8/resources/views/product/index.blade.php
@@ -3,7 +3,21 @@
 @section('title', '商品一覧画面')
 
 @section('search_box')
-<p>検索ボックス</p>
+    <form action = "{{ route('product.index') }}" method = "GET">
+        @csrf
+        <!-- 商品名の部分一致検索用入力フォーム -->
+        <input type = "text" name = "keyword" value = "{{ request('keyword') }}" placeholder = "検索キーワード">
+
+        <!-- メーカー名での絞り込み用セレクトボックス -->
+        <select name = "company">
+            <option value = "">メーカー名</option>
+            @foreach($items as $item)
+                <option value = "{{ $company->id }}" {{ request('company_id') == $company->id ? 'selected' : ''}}>
+                    {{ $company->name }}
+                </option>
+            @endforeach
+        </select>
+    </form>
 @endsection
 
 @section('content')
@@ -28,7 +42,7 @@
                     <td>￥{{ $item->price }}</td> <!-- 価格 -->
                     <td>{{ $item->stock }}</td> <!-- 在庫数 -->
                     <td>{{ $item->company->company_name }}</td> <!-- メーカー名 -->
-                    <td><a href = "{{ route('product.show', ['id' => $item->id]) }}">詳細</a></td> <!-- 詳細ボタン -->
+                    <td><a href = "{{ route('product.show', ['id' => $item->id]) }}">詳細</></td> <!-- 詳細ボタン -->
                     <td>
                         <form action = "{{ route('product.destroy', ['id' => $item->id]) }}" method = "POST">
                             @method('DELETE')
