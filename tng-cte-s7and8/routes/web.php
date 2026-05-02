2f94c626a4ed04bd3455c5458eeb9c3fa80568fd
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-02 17:20:15 +0900

    ルート情報を編集
    理由
    * 全ページ分のルート情報を追記。
    
    作業ファイル
    * modified:   routes/web.php

--- tng-cte-s7and8/routes/web.php
+++ tng-cte-s7and8/routes/web.php
@@ -12,6 +12,25 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/product', [ProductController::class, 'index'])->name('index');
-// Route::post('/product', [ProductController::class, 'index']);
-Route::get('/product/add', [ProductController::class, 'add'])->name('add');
+// --- 商品管理機能 ---
+
+// 商品一覧・検索
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+
+// 商品新規登録(入力)
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+
+// 商品新規登録(処理)
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+
+// 商品情報詳細
+Route::get('/product/{item}', [ProductController::class, 'show'])->name('product.show');
+
+// 商品情報編集(入力)
+Route::get('/product/{item}/edit', [ProductController::class, 'edit'])->name('product.edit');
+
+// 商品情報編集(処理)
+Route::put('/product/{item}', [ProductController::class, 'update'])->name('product.update');
+
+// 削除
+Route::delete('/product/{item}', [ProductController::class, 'destroy'])->name('product.destroy');
