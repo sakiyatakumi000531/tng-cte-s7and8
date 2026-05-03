82f65682e2045bf2bbe43f0d5c2e4ed65c466a0f
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-03 22:21:32 +0900

    各Factoryのメソッドチェーンにunique()を追記
    理由
    * companiesテーブルのidとcompany_nameの組み合わせが一意でなく、セレクトボックスでの絞り込みに重複する値があったため
    
    作業ファイル
    * modified:   database/factories/CompanyFactory.php
    * modified:   database/factories/ProductFactory.php

--- tng-cte-s7and8/database/factories/CompanyFactory.php
+++ tng-cte-s7and8/database/factories/CompanyFactory.php
@@ -20,11 +20,11 @@ class CompanyFactory extends Factory
         return [
             // 主キーは設定不要
             // メーカー名(string)
-            'company_name' => $this->faker->company(),
+            'company_name' => $this->faker->unique()->company(),
             // 住所(string)
-            'street_address' => $this->faker->address(),
+            'street_address' => $this->faker->unique()->address(),
             // 代表者名(string)
-            'representative_name' => $this->faker->name(),
+            'representative_name' => $this->faker->unique()->name(),
             // 作成日は1年前から現在時刻の間でランダム生成。format()不要と思われるが、念のためformat()しておく
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now')->format('Y-m-d H:i:s'),
             // 更新日は現在時刻で固定。format()不要
