cacaa4c57530253d95cb94ace9d7786f43e4d5b4
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-06 21:27:14 +0900

    クラスを付与したdivを配置
    理由
    * form内部の部品のレイアウトを変更させるため
    
    作業ファイル
    * modified:   resources/views/products/create.blade.php
    * modified:   resources/views/products/fields.blade.php

--- tng-cte-s7and8/resources/views/products/fields.blade.php
+++ tng-cte-s7and8/resources/views/products/fields.blade.php
@@ -12,64 +12,66 @@
     これにより、1つのファイルで「登録(直前の入力なし)」と「編集(controllerから渡される値あり)」の両方に対応できる
 -->
 
-<!-- 商品名 -->
-<label for = "product_name">商品名</label>
-<input type = "text" name = "product_name" id = "product_name" value = "{{ old('product_name', $product->product_name ?? '') }}">
+<div class = "form-container"> <!-- FlexBoxで子要素のレイアウトを変更させるためのdiv -->
 
-@error('product_name')
-<div class = "error">
-    {{ $message }}
-</div>
-@enderror
+    <!-- 商品名 -->
+    <label for = "product_name">商品名</label>
+    <input type = "text" name = "product_name" id = "product_name" value = "{{ old('product_name', $product->product_name ?? '') }}">
 
-
-<!-- メーカー名 -->
-<label for = "company_id">メーカー名</label>
-<select name = "company_id" id = "company_id">
-        <option value = "">選択してください</option>
-    @foreach($companies as $company)
-        <!-- value属性以後はselectedを判定するための条件判定 -->
-        <option value = "{{ $company->id }}" {{ old('company_id', $product->company_id ?? '') == $company->id ? 'selected' : ''}}>
-            {{ $company->company_name }}
-        </option>
-    @endforeach
-</select>
-
-@error('company_id')
-<div class = "error">
-    {{ $message }}
-</div>
-@enderror
-
-
-<!-- 価格 -->
-<label for = "price">価格</label>
-<input type = "number" name = "price" id = "price" value = "{{ old('price', $product->price ?? '') }}">
-
-@error('price')
-<div class = "error">
-    {{ $message }}
-</div>
-@enderror
-
-
-<!-- 在庫数 -->
-<label for = "stock">在庫数</label>
-<input type = "number" name = "stock" id = "stock" value = "{{ old('stock', $product->stock ?? '') }}">
-
-@error('stock')
-<div class = "error">
-    {{ $message }}
-</div>
-@enderror
-
-
-<!-- コメント -->
-<label for = "comment">コメント</label>
-<textarea name = "comment" id = "comment">{{ old('comment', $product->comment ?? '') }}</textarea>
-
-@error('comment')
+    @error('product_name')
     <div class = "error">
         {{ $message }}
     </div>
-@enderror
+    @enderror
+
+
+    <!-- メーカー名 -->
+    <label for = "company_id">メーカー名</label>
+    <select name = "company_id" id = "company_id">
+            <option value = "">選択してください</option>
+        @foreach($companies as $company)
+            <!-- value属性以後はselectedを判定するための条件判定 -->
+            <option value = "{{ $company->id }}" {{ old('company_id', $product->company_id ?? '') == $company->id ? 'selected' : ''}}>
+                {{ $company->company_name }}
+            </option>
+        @endforeach
+    </select>
+
+    @error('company_id')
+    <div class = "error">
+        {{ $message }}
+    </div>
+    @enderror
+
+
+    <!-- 価格 -->
+    <label for = "price">価格</label>
+    <input type = "number" name = "price" id = "price" value = "{{ old('price', $product->price ?? '') }}">
+
+    @error('price')
+    <div class = "error">
+        {{ $message }}
+    </div>
+    @enderror
+
+
+    <!-- 在庫数 -->
+    <label for = "stock">在庫数</label>
+    <input type = "number" name = "stock" id = "stock" value = "{{ old('stock', $product->stock ?? '') }}">
+
+    @error('stock')
+    <div class = "error">
+        {{ $message }}
+    </div>
+    @enderror
+
+
+    <!-- コメント -->
+    <label for = "comment">コメント</label>
+    <textarea name = "comment" id = "comment">{{ old('comment', $product->comment ?? '') }}</textarea>
+
+    @error('comment')
+        <div class = "error">
+            {{ $message }}
+        </div>
+    @enderror
