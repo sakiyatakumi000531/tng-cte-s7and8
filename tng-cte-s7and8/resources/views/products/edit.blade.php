8ae3d51fb736abd15669c87c5c23b35a9cf1c0e9
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-10 20:32:35 +0900

    editページのviewを作成
    作業ファイル
    * modified:   resources/views/products/edit.blade.php

---
+++ tng-cte-s7and8/resources/views/products/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.s7and8app')
+
+@section('title', '商品新規登録画面')
+
+@section('content')
+<form action="{{ route('products.update') }}" method="POST" enctype="multipart/form-data">
+    @method('PUT')
+
+    {{-- 現在のURLパラメータをすべて隠しデータで送る --}}
+    @foreach(request()->query() as $key => $value)
+        <input type = "hidden" name = "back_params[{{ key }}]" value = "{{ value }}">
+    @endforeach
+
+    @include('products.fields')
+
+    <!-- 更新ボタン -->
+    <button type = "submit">更新</button>
+
+    <!-- 戻るボタン -->
+    <!-- URLのクエリパラメータを引き継いで検索条件を維持 -->
+    <a href="{{ route('products.index', request()->query()) }}">戻る</a>
+</form>
+@endsection
