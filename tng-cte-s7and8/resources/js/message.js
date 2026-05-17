c6a40858d882fec75ec8b8dc074ca2fd4e6a0218
sakiyatakumi000531 <sakiyatakumi000531@gmail.com>
2026-05-17 23:47:11 +0900

    createページのフォーム送信時にイベントを登録
    理由
    * 確認ダイアログを表示させるため
    
    作業ファイル
    * new file:   resources/js/edit-confirm.js
    * modified:   resources/js/message.js
    * modified:   vite.config.js

--- tng-cte-s7and8/resources/js/message.js
+++ tng-cte-s7and8/resources/js/message.js
@@ -1,2 +1,3 @@
 export const CONFIRM_CREATE_MESSAGE = 'この商品を登録してもよろしいですか？';
+export const CONFIRM_EDIT_MESSAGE = 'この商品の情報を更新してもよろしいですか？';
 export const CONFIRM_DELETE_MESSAGE = 'この商品を削除してもよろしいですか？';
